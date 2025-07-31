24893489de19cb119fbedb759e2cae4f480306af
Aung Thet Hein <soulfire084@gmail.com>
2025-07-31 11:58:33 +0630

    Function tested

--- Admin/index.php
+++ Admin/index.php
@@ -3,6 +3,15 @@
 $batch = 111; // interger data type
 $name = "IMC"; //string data type
 $temp = 33.5; // float data type
-echo "<b><em>mingalar pr bya</em></b>";
+echo "<b><em>mingalar pr bya. Batch $batch ka br</em></b>";
 
+function power($num1, $num2): float|int|object
+{
+    return pow(num: $num1, exponent: $num2);
+}
+
+$number1 = 2;
+$number2 = 6;
+$result = power(num1: $number1, num2: $number2);
+echo "<br>result is $result";
 ?> 
